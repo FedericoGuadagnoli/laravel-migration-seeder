75b6c02b96bf8d61cf3ccb1a56b66e3eb558df73
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-27 16:02:57 +0100

    Create content in table trains

--- database/migrations/2023_02_27_144331_create_trains_table.php
+++ database/migrations/2023_02_27_144331_create_trains_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 150);
+            $table->string('Stazione di arrivo', 150);
+            $table->datetimes('Orario di partenza');
+            $table->datetimes('Orario di arrivo');
+            $table->string('Codice treno', 20);
+            $table->tinyInteger('Numero carrozze')->unsigned();
+            $table->boolean('In orario')->unsigned();
+            $table->boolean('Cancellato')->unsigned();
         });
     }
 
