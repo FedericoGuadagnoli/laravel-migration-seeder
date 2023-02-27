012a75ac81d1110e6bdd42c41a95c4b2d8cfb14a
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-27 18:13:42 +0100

    create content in train seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,14 +5,27 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $new_train = new Train();
+        for ($i = 0; $i < 5; $i++) {
+            $train = new Train();
+            $train->Azienda = $faker->word();
+            $train->Stazione_di_partenza = $faker->word();
+            $train->Stazione_di_arrivo = $faker->word();
+            $train->Orario_di_partenza = $faker->time();
+            $train->Orario_di_arrivo = $faker->time();
+            $train->Codice_treno = $faker->bothify('?????-#####');
+            $train->Numero_carrozze = $faker->randomNumber(2, false);
+            $train->In_orario = $faker->boolean();
+            $train->Cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
